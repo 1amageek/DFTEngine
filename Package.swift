c332d850ac62c6147f1c9ce960f11768b1a2299c
1amageek <tmy0x3@icloud.com>
2026-07-17 09:51:51 +0900

    Update workspace dependency revisions

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let timingEngineDependency: Package.Dependency = FileManager.default.fileExists(
     atPath: workspaceRoot.appendingPathComponent("TimingEngine/Package.swift").path
 )
     ? .package(path: "../TimingEngine")
-    : .package(url: "https://github.com/1amageek/TimingEngine.git", revision: "0fecd6f568c7c21ec98ddc3b96aad8eacac44c8c")
+    : .package(url: "https://github.com/1amageek/TimingEngine.git", revision: "9189b6dba804191d664eeae334fc429fa74ba421")
 
 let pdkKitDependency: Package.Dependency = FileManager.default.fileExists(
     atPath: workspaceRoot.appendingPathComponent("PDKKit/Package.swift").path
@@ -40,7 +40,7 @@ let toolQualificationDependency: Package.Dependency = FileManager.default.fileEx
     atPath: workspaceRoot.appendingPathComponent("ToolQualification/Package.swift").path
 )
     ? .package(path: "../ToolQualification")
-    : .package(url: "https://github.com/1amageek/ToolQualification.git", revision: "1856a1bc5660febbe2f0358d3e5e0262e496b3d3")
+    : .package(url: "https://github.com/1amageek/ToolQualification.git", revision: "81305bc9e603e0fbd6a9bda9084e13d3f59814f0")
 
 let package = Package(
     name: "DFTEngine",
